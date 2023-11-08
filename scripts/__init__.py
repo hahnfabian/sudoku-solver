ab856a549afa31b7364a529f23ab620971cf905b
hahnfabian <hahn.fabian@proton.me>
2023-11-09 00:57:13 +0100

    comment out init

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,4 +1,4 @@
-from .model import get_model
-from .process_data import get_data
+#from .model import get_model
+#from .process_data import get_data
 
-__all__ = ['get_model', 'get_data']
+#__all__ = ['get_model', 'get_data']
